d11b676a5f5cde513cc411489cf6859f085fc441
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-28 14:53:33 -0700

    add event listener to button and validate user name prompt and cancle prompt

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ import { makeItYes } from './make-it-yes.js';
 
 // initialize state
 const button = document.getElementById('promptButton');
-console.log(button);
 
 const results = document.getElementById('view-results');
-console.log(results);
+
 // set event listeners to update state and DOM
+button.addEventListener('click', () => {
+    const userName = prompt('Enter your name?');
+
+    const userConfirmation = confirm('Do you really think you know me well enough to play?');
+
+    if (!userConfirmation){
+        alert('Awww. You didn\'t even try.');
+    }
+    console.log(userName);
+});
